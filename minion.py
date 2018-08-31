27290558f23d95435b5cf489de1001bcf9090198
Will Carhart <wcarhart@sandiego.edu>
2018-08-30 22:19:13 -0700

    Updated minion

---
+++ minion.py
@@ -0,0 +1,33 @@
+import threading
+import sys
+import queue
+
+from subprocess import PIPE
+from subprocess import Popen
+
+class Minion(threading.Thread):
+	def __init__(self, thread_id, input_queue, output_queue, result_queue):
+		threading.Thread.__init__(self)
+		self.thread_id = thread_id
+		self.input_queue = input_queue
+		self.output_queue = output_queue
+		self.result_queue = result_queue
+	def run(self):
+		thread_task(self.thread_id, self.input_queue, self.output_queue, self.result_queue)
+
+def run(thread_id, input_queue, output_queue, result_queue):
+	while True:
+		cmd_id, cmd = input_queue.get()
+		thread_tuple = (thread_id, "Starting thread for CID {}: {}".format(cmd_id, cmd))
+		output_queue.put(thread_tuple)
+
+		proc = Popen(cmd, stdout=PIPE, shell=True)
+		out, err = proc.communicate()
+
+		thread_tuple = (thread_id, "STDOUT from CID {}: ".format(cmd_id) + out)
+		output_queue.put(thread_tuple)
+		thread_tuple = (thread_id, "STDERR from CID {}: ".format(cmd_id) + err)
+		output_queue.put(thread_tuple)
+		input_queue.task_done()
+		result_queue.put(cmd)
+		
